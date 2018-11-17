8786cd8412a305b5b1ed44579e236c03d4a62d47
Jake Spencer <jakesta13@gmail.com>
2018-11-17 18:31:14 +0000

    Corrected comments, made Youtube-dl ignore errors

--- ascript.sh
+++ ascript.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
-## This script works on my findings of a 'security feature' of the wireles$
+## This script works on my findings of a 'security feature' of the wireless SDcard reader.. read more on the github project.
 ## non-privalaged username and password is 'user'
 ## privalaged username and password is 'root'
-## Fun-fact; The gigastone uses Telnet, and you can login as root -- is re$
-## Please note: If running on a raspberry pi, keep in mind that you are wr$
-## you may want to consider running this on a USB of at-least "8GB", depen$
+## Fun-fact; The gigastone uses Telnet, and you can login as root.
+## Please note: If running on a raspberry pi, keep in mind that you are writing and deleting from the SD card
+## you may want to consider running this on a USB of at-least "8GB", depends on what your primary content consists of
+## (E.g 30 minute documenteries, 3 minute videos, Public-domain movies, etc)
 ## https://github.com/Jakesta13
 
-# Specifiy script directory where this script is located, leave it as a peri$
-# manually in the current directory. (E.g this script could be used in a c$
-SCRIPT_DIR=.
+# Specifiy script directory where this script is located.
+# This makes it simpler to be used in a cron job
+SCRIPT_DIR=
 
 # Specifiy the base direcotry where the vidoes are downloaded, for now this is a workaround.
 # WARNING NOTICE: MAKE SURE YOU SPECIFY THE CORRECT DIRECTORY AS IT WILL DELETE ANYTHING THAT ISNT A BASH SCRIPT (.sh)
 # Please use inital forward slahes, this is so there is no automated mistakes.
-BASE_DIR=.
+BASE_DIR=
 
-# Just in-case your gigastone device has a different internal IP, you can $
+# Just in-case your gigastone device has a different internal IP, you can change it here
 GIP=192.168.1.2
 
 # Folder to put the videos in gigastone.
-# Note: you don't need a inital and a  final forward slash as the script a$
-# Bonus: default location is on the SDcard, you'll have to do some looking$
+# Note: script assumes you want to place the vides on the internal SDcard that you place into the reader
+# You will need to manually modify the areas where it does this. (/SD/.sda1/)
 GDIR=Youtube
 
 # IFTTT Private Key and Applet name.
-# You can use IFTTT to get a notification when the download is done, but y$
-# and create an "Applet" called "Webhooks" -- You also need to find your p$
-# You can disable or change the http request at the bottom of the script, $
+# You can use IFTTT to get a notification when the download is done, but you need an account (free) with IFTTT
+# and create an "Applet" called "Webhooks" -- You also need to find your private key.
+# You can disable or change the http request at the bottom of the script, just comment it out.
+# Will add an option here to disable it later on.
 IFTTT_KEY=
 IFTTT_Applet=
 
@@ -37,7 +39,7 @@ if ping -I eth0 -c 1 -W 5 ${GIP} >/dev/null; then
   echo "we have connectivity"
   wget -O "list.txt" ftp://user:user@${GIP}/SD/.sda1/${GDIR}/list.txt
   sleep 1
-  youtube-dl -a list.txt
+  youtube-dl -i -a list.txt
   rm *.part
   rename 's/[^a-zA-Z0-9 .]//g' *.mp4
   rename 's/[^a-zA-Z0-9 .]//g' *.webm
